959034cf50c5188dbae3221538edcafbf4effcb0
AnlayKor <150436643+AnlayKor@users.noreply.github.com>
2026-07-06 15:36:01 +0700

    Update rt_detr.py

--- src/models/rt_detr.py
+++ src/models/rt_detr.py
@@ -1,11 +1,7 @@
-# src/models/rt_detr.py
 from ultralytics import RTDETR
 
 def get_rtdetr_model(weights_name="rtdetr-l.pt"):
-    """
-    Инициализация современной трансформерной архитектуры RT-DETR 
-    на базе движка Ultralytics.
-    """
+
     print(f"[Инициализация] Загрузка архитектуры RT-DETR ({weights_name})...")
     model = RTDETR(weights_name)
     return model
