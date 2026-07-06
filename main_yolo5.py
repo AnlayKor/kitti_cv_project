d3c8cef12e387a47f81e37ccf0897c7681982989
AnlayKor <150436643+AnlayKor@users.noreply.github.com>
2026-07-06 15:28:14 +0700

    Update main_yolo5.py

--- main_yolo5.py
+++ main_yolo5.py
@@ -1,4 +1,3 @@
-# main_yolo5.py
 import argparse
 import torch
 from ultralytics import YOLO
@@ -11,27 +10,22 @@ def main():
     parser.add_argument("--lr", type=float, default=0.01, help="Стартовый Learning Rate (lr0)")
     args = parser.parse_args()
 
-    # Автоматический выбор устройства
     current_device = 0 if torch.cuda.is_available() else 'cpu'
 
     print(f"\n=== ЗАПУСК ЭКСПЕРИМЕНТА YOLOv5 ===")
     print(f"Архитектура: YOLOv5n | Эпох: {args.epochs} | Batch Size: {args.batch} | LR: {args.lr}")
     print(f"Используемое устройство: {current_device}\n")
 
-    # Загружаем предобученную на COCO модель YOLOv5 (нано-версия)
-    # Ultralytics автоматически подтянет её структуру
     model = YOLO("yolov5n.pt")
 
-    # Имя папки для результатов
     experiment_name = f"yolov5n_lr_{args.lr}_batch_{args.batch}"
 
-    # Запуск честного обучения по нашему датасету KITTI
     model.train(
-        data="configs/default.yaml",  # Твой готовый конфиг путей и классов
+        data="configs/default.yaml",
         epochs=args.epochs,
-        imgsz=640,  # Стандартное разрешение для YOLO
+        imgsz=640,
         device=current_device,
-        project="results",  # Все результаты сложатся в общую папку results
+        project="results",
         name=experiment_name,
         lr0=args.lr,
         batch=args.batch
