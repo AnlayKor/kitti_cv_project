fb5e91942177c08d4e5a64db45ffc3cee3afdb23
AnlayKor <150436643+AnlayKor@users.noreply.github.com>
2026-07-06 15:35:07 +0700

    Update faster_rcnn.py

--- src/models/faster_rcnn.py
+++ src/models/faster_rcnn.py
@@ -1,4 +1,3 @@
-# src/models/faster_rcnn.py
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
@@ -9,10 +8,8 @@ def get_faster_rcnn_model(num_classes=8):
     """
     print(f"[Инициализация] Загрузка оригинальной архитектуры Faster R-CNN (ResNet-50-FPN)...")
     
-    # Загружаем предобученную модель
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     
-    # Меняем голову классификатора под наше количество классов (в KITTI их 8)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     
